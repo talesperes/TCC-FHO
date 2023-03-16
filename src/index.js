0353caa0d20256ee69324afd5af929618b171572
Tales Oliveira <tales.oliveira@alunos.fho.edu.br>
2023-03-15 21:29:08 -0300

    fix main css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
 import reportWebVitals from "./reportWebVitals";
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
